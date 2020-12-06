8ab4f2e241e17a43c8c954a80b4ae49e742a4e44
Atef Arfaoui <atef@kaptio.com>
2020-12-06 19:11:28 +0100

    Day5

---
+++ day5/5.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//file, err := os.Open("2020/day5/input_small.txt")
+	file, err := os.Open("2020/day5/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	var max int64 = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		firsPart := line[:len(line)-3]
+		secondPart := line[len(line)-3:]
+		firsPartBinary := intToBinary(strings.ReplaceAll(strings.ReplaceAll(firsPart, "B", "1"), "F", "0"))
+		secondPartBinary := intToBinary(strings.ReplaceAll(strings.ReplaceAll(secondPart, "R", "1"), "L", "0"))
+		if firsPartBinary*8+secondPartBinary > max {
+			max = firsPartBinary*8 + secondPartBinary
+		}
+	}
+	fmt.Println("Max sit", max)
+
+}
+
+func intToBinary(part string) int64 {
+	interger, _ := strconv.ParseInt(part, 2, 64)
+	return interger
+}
